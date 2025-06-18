56c47129c2d44a3d5b22ba25f905a1202ad35b92
Steven Song <steven.song98@gmail.com>
2025-06-18 11:49:11 -0500

    mlm tweak

--- scripts/train-chexpertplus-gloria-frontal-impression-igl-tg-mlm.sh
+++ scripts/train-chexpertplus-gloria-frontal-impression-igl-tg-mlm.sh
@@ -17,7 +17,7 @@ python train.py fit \
 --config configs/$NAME.yaml \
 --model.checkpoint_path /opt/gpudata/remix/$NAME-$VERSION \
 --model.loss_combo $LOSS_COMBO \
---model.mlm_probability $MLM_P \
 --data.section $SECTION \
+--data.mlm_probability $MLM_P \
 --trainer.logger.name $NAME \
 --trainer.logger.version $VERSION
